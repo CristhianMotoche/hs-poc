9f57b3d05153a471341e7fb4b138775fa63d5710
CristhianMotoche <cristhian.motoche@gmail.com>
2023-06-21 21:18:43 -0500

    feat: Basic usage of htaglib

--- taggy/README.md
+++ taggy/README.md
@@ -1,3 +1,12 @@
 # Taggy
 
 Testing htaglib to tag my music.
+
+## Requirements
+
+* TagLib C libraries
+
+```
+# On Ubuntu
+sudo apt install libtaglib0-dev
+```
--- taggy/app/Main.hs
+++ taggy/app/Main.hs
@@ -1,4 +1,33 @@
-module Main where
+module Main (main) where
+
+import Data.Monoid
+import Sound.HTagLib
+import System.Environment (getArgs)
+
+data AudioTrack = AudioTrack
+  { atTitle :: Title,
+    atArtist :: Artist,
+    atAlbum :: Album,
+    atComment :: Comment,
+    atGenre :: Genre,
+    atYear :: Maybe Year,
+    atTrack :: Maybe TrackNumber
+  }
+  deriving (Show)
+
+audioTrackGetter :: TagGetter AudioTrack
+audioTrackGetter =
+  AudioTrack
+    <$> titleGetter
+    <*> artistGetter
+    <*> albumGetter
+    <*> commentGetter
+    <*> genreGetter
+    <*> yearGetter
+    <*> trackNumberGetter
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  path <- head <$> getArgs
+  track <- getTags path audioTrackGetter
+  print track
