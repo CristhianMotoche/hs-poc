c976b903a8f0e14b9b3e943f33b3993d0d6d0e05
CristhianMotoche <cristhian.motoche@gmail.com>
2024-06-19 21:26:47 -0500

    feat(pc): Initial types

--- pa-comer/app/Main.hs
+++ pa-comer/app/Main.hs
@@ -1,4 +1,34 @@
 module Main where
 
+newtype Ingredient = Ingredient
+  { iName :: String
+  }
+
+data Proportion = Proportion
+  { pQuantity :: Int,
+    pIngredient :: Ingredient
+  }
+
+data Recipe = Recipe
+  { rName :: String,
+    rProportions :: [Proportion]
+  }
+
+recipies :: [Recipe]
+recipies =
+  [ Recipe
+      { rName = "Tigrillo",
+        rProportions =
+          [ Proportion
+              { pQuantity = 1,
+                pIngredient =
+                  Ingredient
+                    { iName = "Plantain"
+                    }
+              }
+          ]
+      }
+  ]
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
