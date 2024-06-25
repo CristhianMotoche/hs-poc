5e550525815cec65d89b062a9062f5474db98e09
CristhianMotoche <cristhian.motoche@gmail.com>
2024-06-24 20:09:53 -0500

    feat(pc): Serve HTML

--- pa-comer/app/Main.hs
+++ pa-comer/app/Main.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
+
+import Data.ByteString.Lazy as Lazy
+import Data.Proxy
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai.Handler.Warp (run)
+import Servant.API
+import Servant.Server
+
+-- ^ Servant stuff
+
+data HTML = HTML
+
+newtype RawHtml = RawHtml {unRaw :: Lazy.ByteString}
+
+instance Accept HTML where
+  contentType _ = "text" // "html" /: ("charset", "utf-8")
+
+instance MimeRender HTML RawHtml where
+  mimeRender _ = unRaw
+
+type MealsAPI = "meals" :> Get '[HTML] RawHtml
+-- ^ My types
 
 newtype Ingredient = Ingredient
   { iName :: String
@@ -30,5 +57,17 @@ recipies =
       }
   ]
 
+mealHandler :: Handler RawHtml
+mealHandler = do
+  return (RawHtml $ "<body><i>Hello</i> <strong>World</strong></body>")
+
+myServer :: Server MealsAPI
+myServer = mealHandler
+
+port :: Int
+port = 8080
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn $ "Server listening on port " <> show port <> "..."
+  run port (serve (Proxy :: Proxy MealsAPI) myServer)
