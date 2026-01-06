6d862dda38935c2cefafbec38d20808811800ebe
CristhianMotoche <cristhian.motoche@gmail.com>
2026-01-05 22:50:30 -0500

    chore: Now it is working

--- data-analysis/app/Main.hs
+++ data-analysis/app/Main.hs
@@ -1,8 +1,9 @@
 module Main (main) where
 
-import           DataFrame as D
+import DataFrame as D
 
 main :: IO ()
 main = do
   df <- D.readCsv "data/Titanic-Dataset.csv"
-  print df
+  print $ D.columnNames df
+  print $ D.take 5 df
