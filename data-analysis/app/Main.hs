f368f5095115eea67978fbbf0affad0a9f08e4eb
CristhianMotoche <cristhian.motoche@gmail.com>
2026-01-13 19:08:43 -0500

    chore: Ensure we use version 4 of dataframe

--- data-analysis/app/Main.hs
+++ data-analysis/app/Main.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 
-import DataFrame as D
+import           DataFrame as D
 
 main :: IO ()
 main = do
   df <- D.readCsv "data/Titanic-Dataset.csv"
   print $ D.columnNames df
-  print $ D.take 5 df
+  print $ D.describeColumns df
